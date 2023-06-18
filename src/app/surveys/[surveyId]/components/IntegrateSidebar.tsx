bdc1975eaba66aa091ac6e9c27df53896aa53436
Samuel Amoah <sa.am@programmer.net>
2023-06-19 00:42:26 +0200

    create IntegrateSidebar ui

--- src/app/surveys/[surveyId]/components/IntegrateSidebar.tsx
+++ src/app/surveys/[surveyId]/components/IntegrateSidebar.tsx
@@ -1,6 +1,20 @@
 const IntegrateSidebar = () => (
   <div className="flex h-full flex-col">
-    <section className="grow"></section>
+    <section className="grow">
+      <header className=" p-6">
+        <h1>Integrations</h1>
+        <p className="mt-2 text-xs text-slate-600">
+          Configure your favorite app with SurveyFly
+        </p>
+      </header>
+
+      <menu className="mt-12">
+        <li className="pl relative rounded-sm bg-slate-100 py-3">
+          <span className="absolute left-0 top-0 h-full w-1 bg-yellow-500"></span>
+          <span className="ml-5 font-semibold text-slate-600">Webhooks</span>
+        </li>
+      </menu>
+    </section>
     <footer className="flex h-24 bg-indigo-300"></footer>
   </div>
 );
