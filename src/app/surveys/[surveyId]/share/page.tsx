c7e2127f23ce2255986b2c413aeb0de83c61d4e3
Samuel Amoah <sa.am@programmer.net>
2023-06-19 02:33:27 +0200

    create ui for share page

--- src/app/surveys/[surveyId]/share/page.tsx
+++ src/app/surveys/[surveyId]/share/page.tsx
@@ -1,3 +1,49 @@
-const Page = () => <h1>Share page</h1>;
+import { Chain } from "@/ui/icons";
+
+const Page = () => (
+  <div className="flex h-full flex-col items-center gap-3 overflow-y-auto p-10">
+    <article className="flex w-3/5 flex-col gap-3">
+      <header className="">
+        <h1>Share Survey</h1>
+        <p className="">
+          Find various ways to share your survey with the world
+        </p>
+      </header>
+      <p className="relative grid place-content-center rounded-sm bg-yellow-200 p-2 text-sm text-slate-600">
+        <span className="absolute left-0 top-0 h-full w-1 rounded-bl-sm rounded-tl-sm bg-yellow-500"></span>
+        <span>
+          Make sure you <b>publish</b> your survey before sharing.
+        </span>
+      </p>
+      <section className="mt-5 flex flex-col gap-2 rounded-sm p-6 ring-1 ring-slate-300">
+        <label className="text-sm font-medium">Link to share</label>
+        <p className="flex items-center rounded-sm bg-slate-200">
+          <span className="bg-green grid place-content-center p-3">
+            <Chain size={12} />
+          </span>
+          <span className="flex-1 select-all font-mono text-slate-600">
+            https://fill.surveyfly.com/i/2483038948
+          </span>
+        </p>
+        <div className="flex justify-end">
+          <button className="btn bg-purple-500">Copy</button>
+        </div>
+      </section>
+      <section className="mt-5 flex flex-col gap-5 rounded-sm p-6 ring-1 ring-slate-300">
+        <header>
+          <h3>Share on Social Media</h3>
+          <p className="text-sm">
+            One click to share survey on your favorite social media
+          </p>
+        </header>
+        <footer className="flex gap-2">
+          <button className="btn bg-blue-600">Facebook</button>
+          <button className="btn bg-sky-500">Twitter</button>
+          <button className="btn bg-blue-900">LinkedIn</button>
+        </footer>
+      </section>
+    </article>
+  </div>
+);
 
 export default Page;
--- src/ui/icons/index.ts
+++ src/ui/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as Trash } from "./Trash";
+export { default as Chain } from "./Chain";
 export { default as Radio } from "./Radio";
 export { default as Checkbox } from "./Checkbox";
 export { default as ArrowRight } from "./ArrowRight";
@@ -6,5 +7,4 @@ export { default as CloseCircle } from "./CloseCircle";
 export { default as VerticalDots } from "./VerticalDots";
 export { default as ThumbUpThumbDown } from "./ThumbUpThumbDown";
 export { default as CloseCircleOutline } from "./CloseCircleOutline";
-
 export type { IconProps, Icon } from "./IconComponent";
