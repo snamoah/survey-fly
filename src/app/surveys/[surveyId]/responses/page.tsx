c1c522fc3b812854e66f07cf4e33b17aab51a802
Samuel Amoah <sa.am@programmer.net>
2023-06-19 02:33:39 +0200

    create ui for responses page

--- src/app/surveys/[surveyId]/responses/page.tsx
+++ src/app/surveys/[surveyId]/responses/page.tsx
@@ -1,3 +1,29 @@
-const Page = () => <h1>Responses</h1>;
+import Link from "next/link";
+import { Share } from "@/ui/illustrations";
+
+const Page = ({ params: { surveyId } }: { params: { surveyId: string } }) => {
+  return (
+    <div className="grid h-full place-content-center">
+      <section className="flex  flex-col items-center gap-3">
+        <div className="rounded-full bg-sky-200">
+          <Share />
+        </div>
+        <div className="flex w-2/3 flex-col gap-1 text-center">
+          <h2>No responses yet</h2>
+          <p className="text-sm">
+            Start receiving responses to your survey by sharing the link on your
+            various platforms.
+          </p>
+        </div>
+        <Link
+          href={`/surveys/${surveyId}/share`}
+          className="btn bg-orange-500 text-white"
+        >
+          Share survey
+        </Link>
+      </section>
+    </div>
+  );
+};
 
 export default Page;
--- src/ui/illustrations/index.ts
+++ src/ui/illustrations/index.ts
@@ -1,1 +1,2 @@
 export { default as Empty } from "./Empty";
+export { default as Share } from "./Share";
